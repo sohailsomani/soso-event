63f138957ccc2113df45429f6c64755367b260ea
Sohail Somani <me@sohailsomani.com>
2021-01-23 20:47:57 -0500

    Bring in existing implementation

---
+++ soso/event/__init__.py
@@ -0,0 +1,162 @@
+import asyncio
+import collections
+import enum
+import logging
+import typing
+import weakref
+
+
+class EventToken:
+    def __init__(self, value: typing.Any, event: "Event"):
+        self.event: typing.Optional[
+            weakref.ReferenceType["Event"]] = weakref.ref(event)
+        self.value = value
+
+    def disconnect(self) -> None:
+        if self.event is None:
+            return
+        event = self.event()
+        if event is None:
+            return
+        event.disconnectToken(self)
+        self.event = None
+
+    def __del__(self) -> None:
+        self.disconnect()
+
+    def __hash__(self) -> int:
+        return hash(self.value)
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, EventToken) and self.value == other.value
+
+
+class Event:
+    class Group(enum.Enum):
+        STORE = 1
+        PROCESS = 2
+
+    def __init__(self, name: str, *argTypes: type, **kwArgTypes: type):
+        self._name = name
+        self._argTypes = argTypes
+        self._condition = asyncio.Condition()
+        self._kwArgTypes = kwArgTypes
+        self._groups: typing.Dict[
+            Event.Group,
+            typing.List[EventToken]] = collections.defaultdict(list)
+        self._nextEventToken: int = 0
+        self._tokenMap: typing.Dict[EventToken,
+                                    typing.Tuple[typing.Callable[[typing.Any],
+                                                                 typing.Any],
+                                                 Event.Group]] = dict()
+        self._onDisconnect: typing.Optional[typing.Callable[["Event"],
+                                                            None]] = None
+        self._nerrors = 0
+        self._logger = logging.getLogger(__name__)
+        self.__isBeingEmitted = False
+
+    def __len__(self) -> int:
+        return len(self._tokenMap)
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def condition(self) -> asyncio.Condition:
+        return self._condition
+
+    def getHandlers(
+            self) -> typing.List[typing.Callable[[typing.Any], typing.Any]]:
+        return [
+            self._tokenMap[token][0]
+            for token in self._groups[Event.Group.STORE] +
+            self._groups[Event.Group.PROCESS]
+        ]
+
+    def callHandler(self, f: typing.Callable[..., typing.Any], *a: typing.Any,
+                    **kw: typing.Any) -> None:
+        # self._logger.debug("Calling %s",f)
+        if self.__isBeingEmitted:
+            return  # avoid recursion
+        self.__isBeingEmitted = True
+        try:
+            ret = f(*a, **kw)
+            awaitable = asyncio.iscoroutinefunction(f) \
+                or asyncio.iscoroutine(f) \
+                or asyncio.iscoroutinefunction(ret) \
+                or asyncio.iscoroutine(ret)
+            if awaitable:
+                asyncio.get_event_loop().create_task(ret)
+            self._nerrors = 0
+        except Exception as e:
+            self._nerrors += 1
+            import traceback
+            self._logger.error("Error calling %s: %s", f, str(e))
+            self._logger.debug("Error calling %s(*%s,**%s): %s", f, a, kw,
+                               str(e))
+            if self._nerrors == 1:
+                self._logger.error(traceback.format_exc())
+            else:
+                self._logger.debug(traceback.format_exc())
+        finally:
+            self.__isBeingEmitted = False
+
+    def __call__(self, *a, **kw) -> None:  # type: ignore
+        self._logger.debug("Emitting %s", self)
+        # self._logger.debug("Emitting %s(%s,%s)",self,a,kw)
+        self._validateArgs(a, kw)
+        asyncio.get_event_loop().create_task(self.__emitCondition())
+        for f in self.getHandlers():
+            self.callHandler(f, *a, **kw)
+
+    async def __emitCondition(self) -> None:
+        async with self.condition:
+            self.condition.notify_all()
+
+    def _validateArgs(self, args: typing.Iterable[typing.Any],
+                      kwargs: typing.Mapping[str, typing.Any]) -> None:
+        pass
+
+    def emit(self, *a: typing.Any, **kw: typing.Any) -> None:
+        self(*a, **kw)
+
+    def connect(self, callback: typing.Callable[..., typing.Any],
+                group: Group) -> EventToken:
+        for token, (cb, grp) in self._tokenMap.items():
+            if callback == cb:
+                return token
+        self._logger.debug("Connecting %s to %s", self, callback)
+        token = self._newEventToken()
+        self._tokenMap[token] = (callback, group)
+        self._groups[group].append(token)
+        return token
+
+    def onDisconnect(self, callback: typing.Callable[["Event"], None]) -> None:
+        self._onDisconnect = callback
+
+    def _newEventToken(self) -> EventToken:
+        self._nextEventToken += 1
+        return EventToken(self._nextEventToken, self)
+
+    def disconnectToken(self, token: EventToken) -> None:
+        if token not in self._tokenMap:
+            return
+        _, group = self._tokenMap[token]
+        del self._tokenMap[token]
+        self._logger.debug("Disconnecting %s token: %s", self, token)
+        self._groups[group] = [a for a in self._groups[group] if a != token]
+        if self._onDisconnect is not None:
+            self._onDisconnect(self)
+
+    def __del__(self) -> None:
+        items = list(self._tokenMap.items())
+        for token, group in items:
+            self.disconnectToken(token)
+
+    @property
+    def count(self) -> int:
+        return sum([len(g) for g in self._groups.values()])
+
+    def __repr__(self) -> str:
+        return f'#<Event {self._name} numListeners={self.count}>'
--- tests/test_event.py
+++ tests/test_event.py
@@ -1,6 +1,63 @@
+import asyncio
 import unittest
+from unittest import mock
+
+from soso.event import Event
 
 
 class TestEvent(unittest.TestCase):
-    def test_something(self) -> None:
-        pass
+    def test_KeywordArgument(self) -> None:
+        event = Event("Test", arg1=int, arg2=float)
+        cb = mock.Mock()
+        event.connect(cb, Event.Group.PROCESS)
+        event(arg2=1.1, arg1=1)
+        cb.assert_called_with(arg1=1, arg2=1.1)
+
+    def test_KeywordArgument2(self) -> None:
+        event = Event("Test", arg1=int, arg2=float)
+        cb = mock.Mock()
+        event.connect(cb, Event.Group.PROCESS)
+        event(1, 1.1)
+        cb.assert_called_with(1, 1.1)
+
+    def test_len(self) -> None:
+        event = Event("Test")
+        self.assertEqual(len(event), 0)
+        cb = mock.Mock()
+        token = event.connect(cb, Event.Group.PROCESS)
+        self.assertEqual(len(event), 1)
+        token.disconnect()
+        self.assertEqual(len(event), 0)
+
+    async def asyncFunc(self) -> None:
+        self.called = True
+
+    def test_lambdaReturningAsync(self) -> None:
+        event = Event("Test")
+        event.connect(lambda: self.asyncFunc(), Event.Group.STORE)
+        event.emit()
+        self.called = False
+        asyncio.get_event_loop().stop()
+        asyncio.get_event_loop().run_forever()
+        self.assertTrue(self.called)
+
+    def test_avoidingRecursion(self) -> None:
+        event = Event("Test")
+        event.connect(event.emit, Event.Group.STORE)
+        event.emit()
+
+    async def waitForEvent(self, event: Event) -> None:
+        async with event.condition:
+            await event.condition.wait()
+        self.called = True
+        asyncio.get_event_loop().stop()
+
+    def test_EventAsCondition(self) -> None:
+        event = Event("Test")
+        self.called = False
+
+        asyncio.get_event_loop().create_task(self.waitForEvent(event))
+        asyncio.get_event_loop().call_soon(event.emit)
+        asyncio.get_event_loop().run_forever()
+
+        self.assertTrue(self.called)
